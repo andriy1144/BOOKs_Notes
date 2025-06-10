1cd7f8b7e2d67103da70be6e05461d503afaeebe
Andriy Murhan <andrijmurgan@gmail.com>
2025-06-10 23:24:42 +0300

    Initial commmit

---
+++ src/indes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on the port ${PORT}`);
+});
